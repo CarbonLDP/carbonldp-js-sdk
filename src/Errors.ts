05220773a6b4a8bb82f0569211a52faba624c081
Miguel Aragón <miguel.aragon@base22.com>
2015-10-22 10:18:05 -0500

    Add missing file Errors.js

---
+++ src/Errors.ts
@@ -0,0 +1,10 @@
+import IllegalStateError from './Errors/IllegalStateError';
+import IllegalArgumentError from './Errors/IllegalArgumentError';
+
+
+//@formatter:off
+export {
+	IllegalStateError,
+	IllegalArgumentError
+};
+//@formatter:on
