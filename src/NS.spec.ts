3c8e13471765cb3c817f5f3ba189f6c47fb77520
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2016-03-09 10:01:02 -0600

    Add tests for Carbon/NS file

---
+++ src/NS.spec.ts
@@ -0,0 +1,84 @@
+/// <reference path="./../typings/typings.d.ts" />
+
+import {
+	INSTANCE,
+	STATIC,
+
+	module,
+
+	isDefined,
+	reexports
+} from "./test/JasmineExtender";
+import * as Utils from "./Utils";
+
+import * as C from "./NS/C";
+import * as CP from "./NS/CP";
+import * as CS from "./NS/CS";
+import * as LDP from "./NS/LDP";
+import * as RDF from "./NS/RDF";
+import * as XSD from "./NS/XSD";
+
+import * as NS from "./NS";
+
+describe( module( "NS" ), ():void => {
+
+	it( isDefined(), ():void => {
+		expect( NS ).toBeDefined();
+		expect( Utils.isObject( NS ) ).toBe( true );
+	});
+
+	it( reexports(
+		STATIC,
+		"C",
+		"Carbon/NS/C"
+	), ():void => {
+		expect( NS.C ).toBeDefined();
+		expect( NS.C ).toBe( C );
+	});
+
+	it( reexports(
+		STATIC,
+		"CP",
+		"Carbon/NS/CP"
+	), ():void => {
+		expect( NS.CP ).toBeDefined();
+		expect( NS.CP ).toBe( CP );
+	});
+
+	it( reexports(
+		STATIC,
+		"CS",
+		"Carbon/NS/CS"
+	), ():void => {
+		expect( NS.CS ).toBeDefined();
+		expect( NS.CS ).toBe( CS );
+	});
+
+	it( reexports(
+		STATIC,
+		"LDP",
+		"Carbon/NS/LDP"
+	), ():void => {
+		expect( NS.LDP ).toBeDefined();
+		expect( NS.LDP ).toBe( LDP );
+	});
+
+	it( reexports(
+		STATIC,
+		"RDF",
+		"Carbon/NS/RDF"
+	), ():void => {
+		expect( NS.RDF ).toBeDefined();
+		expect( NS.RDF ).toBe( RDF );
+	});
+
+	it( reexports(
+		STATIC,
+		"XSD",
+		"Carbon/NS/XSD"
+	), ():void => {
+		expect( NS.XSD ).toBeDefined();
+		expect( NS.XSD ).toBe( XSD );
+	});
+
+});
