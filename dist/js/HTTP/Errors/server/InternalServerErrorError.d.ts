e1f680891f3260614b8738d50e0a4f9b1e0c7e51
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2016-03-15 09:03:12 -0600

    Set corrections after merge

--- dist/js/HTTP/Errors/server/InternalServerErrorError.d.ts
+++
@@ -1,6 +0,0 @@
-import HTTPError from "./../HTTPError";
-declare class InternalServerErrorError extends HTTPError {
-    static statusCode: number;
-    name: string;
-}
-export default InternalServerErrorError;
