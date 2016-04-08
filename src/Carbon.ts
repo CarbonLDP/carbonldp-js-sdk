418f98331b9cca9c76fb1ba0ac80943b99e9c47c
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2016-04-08 18:06:19 -0500

    Increase version number

--- src/Carbon.ts
+++ src/Carbon.ts
@@ -64,7 +64,7 @@ class Carbon extends AbstractContext {
 	/* tslint:enable: variable-name */
 
 	// TODO: Get package.json version directly
-	static get version():string { return "0.23.2-ALPHA"; }
+	static get version():string { return "0.24.0-ALPHA"; }
 
 	apps:Apps.Class;
 	get version():string { return Carbon.version; }
