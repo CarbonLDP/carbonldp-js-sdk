a6fe86ec2251ef595ebd6c0f7601ff2b1171e980
Rodolfo Aguirre <aguirreg.rodolfo@gmail.com>
2016-07-14 12:00:36 -0500

    Fix interface of RDFSource

--- dist/LDP/RDFSource.d.ts
+++ dist/LDP/RDFSource.d.ts
@@ -4,10 +4,6 @@ import * as Pointer from "./../Pointer";
 export declare const RDF_CLASS: string;
 export declare const SCHEMA: ObjectSchema.Class;
 export interface Class extends Document.Class {
-    created: Date;
-    modified: Date;
-    defaultInteractionModel: Pointer.Class;
-    accessPoints: Pointer.Class[];
-    accessControlList: Pointer.Class;
+    defaultInteractionModel?: Pointer.Class;
 }
 export default Class;
--- src/LDP/RDFSource.ts
+++ src/LDP/RDFSource.ts
@@ -30,11 +30,7 @@ export const SCHEMA:ObjectSchema.Class = {
 };
 
 export interface Class extends Document.Class {
-	created:Date;
-	modified:Date;
-	defaultInteractionModel:Pointer.Class;
-	accessPoints:Pointer.Class[];
-	accessControlList:Pointer.Class;
+	defaultInteractionModel?:Pointer.Class;
 }
 
 export default Class;
