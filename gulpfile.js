ca8f1a1e46ecd39fb66c82700e21e371b1250bfe
Miguel Aragón <miguel.aragon@base22.com>
2016-10-04 11:22:39 -0500

    Fix merge conflicts and rename gulp tasks to follow convention

--- gulpfile.js
+++ gulpfile.js
@@ -40,61 +40,48 @@ let config = {
 	},
 };
 
-gulp.task( "ts-lint", () => {
-	return gulp.src( config.source.typescript )
-		.pipe( tslint( {
-			tslint: require( "tslint" )
-		} ) )
-		.pipe( tslint.report( "prose" ) )
-		;
+gulp.task( "default", [ "build" ] );
+
+gulp.task( "build", ( done ) => {
+	runSequence(
+		"clean:dist",
+		[ "compile:typescript", "compile:documentation", "bundle:sfx", "prepare:npm-package" ],
+		"finish",
+		done
+	);
 } );
 
-gulp.task( "test:browser", ( done ) => {
-	new karma.Server( {
-		configFile: __dirname + "/karma.conf.js",
-		singleRun: true
-	}, done ).start();
+// "build" task isn't exiting after finishing everything it needs to do
+// For now this task will finish the process
+// TODO: Find the real culprit
+gulp.task( "finish", () => {
+	process.exit( 0 );
 } );
 
-gulp.task( "test:debug", ( done ) => {
-	new karma.Server( {
-		configFile: __dirname + "/karma.conf.js",
-		autoWatch: true,
-		singleRun: false
-	}, done ).start();
+gulp.task( "bundle:sfx", ( done ) => {
+	let builder = new Builder();
+	builder.buildStatic( "build/sfx.js", config.dist.sfxBundle, {
+		"sourceMaps": "inline",
+		"mangle": false,
+		"lowResSourceMaps": false,
+		"removeComments": true,
+	} ).then( () => {
+		done();
+	} ).catch( ( error ) => {
+		util.log( error );
+		done( error );
+	} );
+} );
+
+gulp.task( "clean:dist", ( done ) => {
+	return del( [ config.dist.all, config.dist.doc ], done );
 } );
 
 gulp.task( "clean:temp", () => {
 	del.sync( config.dist.temp );
 } );
 
-gulp.task( "test:node:compile", [ "clean:temp" ], () => {
-	let tsProject = ts.createProject( "tsconfig.json" );
-	let tsResults = gulp.src( config.source.all )
-		.pipe( ts( tsProject ) );
-
-	return tsResults.js
-		.pipe( gulp.dest( config.dist.temp ) );
-} );
-
-gulp.task( "test:node:exec", [ "test:node:compile" ], () => {
-	return gulp.src( config.dist.temp + config.source.test )
-		.pipe( jasmine( {
-			includeStackTrace: true
-		} ) );
-} );
-
-gulp.task( "test:node", ( done ) => {
-	runSequence(
-		"test:node:exec",
-		"clean:temp",
-		done
-	);
-} );
-
-gulp.task( "test", [ "test:browser", "test:node" ] );
-
-gulp.task( "generate-doc", ( done ) => {
+gulp.task( "compile:documentation", ( done ) => {
 	new karma.Server( {
 		configFile: __dirname + "/karma.conf.js",
 		reporters: [ "markdown" ],
@@ -107,7 +94,7 @@ gulp.task( "generate-doc", ( done ) => {
 	}, done ).start();
 } );
 
-gulp.task( "compile-library", () => {
+gulp.task( "compile:typescript", () => {
 	let tsProject = ts.createProject( "tsconfig.json", {
 		"declaration": true,
 	} );
@@ -126,29 +113,25 @@ gulp.task( "compile-library", () => {
 		;
 } );
 
-gulp.task( "bundle-sfx", ( done ) => {
-	let builder = new Builder();
-	builder.buildStatic( "build/sfx.js", config.dist.sfxBundle, {
-		"sourceMaps": "inline",
-		"mangle": false,
-		"lowResSourceMaps": false,
-		"removeComments": true,
-	} ).then( () => {
-		done();
-	} ).catch( ( error ) => {
-		util.log( error );
-		done( error );
-	} );
+gulp.task( "lint", [ "lint:typescript" ] );
+
+gulp.task( "lint:typescript", () => {
+	return gulp.src( config.source.typescript )
+		.pipe( tslint( {
+			tslint: require( "tslint" )
+		} ) )
+		.pipe( tslint.report( "prose" ) )
+		;
 } );
 
-gulp.task( "prepare-npm-package", ( done ) => {
+gulp.task( "prepare:npm-package", ( done ) => {
 	runSequence(
-		[ "prepare-npm-package:copy-docs", "prepare-npm-package:copy-package-json" ],
+		[ "prepare:npm-package|copy:docs", "prepare:npm-package|copy:package-json" ],
 		done
 	);
 } );
 
-gulp.task( "prepare-npm-package:copy-docs", () => {
+gulp.task( "prepare:npm-package|copy:docs", () => {
 	return gulp.src( [
 		"README.md",
 		"CHANGELOG.md",
@@ -156,7 +139,7 @@ gulp.task( "prepare-npm-package:copy-docs", () => {
 	] ).pipe( gulp.dest( config.dist.tsOutput ) );
 } );
 
-gulp.task( "prepare-npm-package:copy-package-json", () => {
+gulp.task( "prepare:npm-package|copy:package-json", () => {
 	return gulp.src( "package.json" )
 		.pipe( jeditor( ( json ) => {
 			delete json.private;
@@ -176,24 +159,43 @@ gulp.task( "prepare-npm-package:copy-package-json", () => {
 	;
 } );
 
-gulp.task( "clean:dist", ( done ) => {
-	return del( [ config.dist.all, config.dist.doc ], done );
+gulp.task( "test", [ "test:browser", "test:node" ] );
+
+gulp.task( "test:browser", ( done ) => {
+	new karma.Server( {
+		configFile: __dirname + "/karma.conf.js",
+		singleRun: true
+	}, done ).start();
 } );
 
-gulp.task( "lint", [ "ts-lint" ] );
+gulp.task( "test:debug", ( done ) => {
+	new karma.Server( {
+		configFile: __dirname + "/karma.conf.js",
+		autoWatch: true,
+		singleRun: false
+	}, done ).start();
+} );
 
-gulp.task( "build", ( done ) => {
+gulp.task( "test:node", ( done ) => {
 	runSequence(
-		"clean:dist",
-		[ "compile-library", "generate-doc", "bundle-sfx", "prepare-npm-package" ],
-		"finish",
+		"test:node|exec",
+		"clean:temp",
 		done
 	);
 } );
 
-// "build" task isn't exiting after finishing everything it needs to do
-// For now this task will finish the process
-// TODO: Find the real culprit
-gulp.task( "finish", () => {
-	process.exit( 0 );
+gulp.task( "test:node|compile", [ "clean:temp" ], () => {
+	let tsProject = ts.createProject( "tsconfig.json" );
+	let tsResults = gulp.src( config.source.all )
+		.pipe( ts( tsProject ) );
+
+	return tsResults.js
+		.pipe( gulp.dest( config.dist.temp ) );
+} );
+
+gulp.task( "test:node|exec", [ "test:node|compile" ], () => {
+	return gulp.src( config.dist.temp + config.source.test )
+		.pipe( jasmine( {
+			includeStackTrace: true
+		} ) );
 } );
