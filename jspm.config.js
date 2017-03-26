8060c00d3695cf7f1c623e2fa7ffb1f04c8ddf5e
Miguel Aragón <miguel.aragon@base22.com>
2017-03-25 20:11:39 -0600

    Update sparqler to 0.1.1

--- jspm.config.js
+++ jspm.config.js
@@ -62,7 +62,7 @@ SystemJS.config({
     "npm:*.json"
   ],
   map: {
-    "sparqler": "npm:sparqler@0.1.0",
+    "sparqler": "npm:sparqler@0.1.1",
     "typescript": "npm:typescript@2.0.6",
     "net": "npm:jspm-nodelibs-net@0.2.0",
     "os": "npm:jspm-nodelibs-os@0.2.0",
