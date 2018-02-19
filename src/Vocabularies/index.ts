6436914fdc96241810ea2d90912552045b47f301
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-02-19 13:14:22 -0600

    Add missing vocabularies index

---
+++ src/Vocabularies/index.ts
@@ -0,0 +1,17 @@
+import * as CS from "./CS";
+import * as LDP from "./LDP";
+import * as RDF from "./RDF";
+import * as SHACL from "./SHACL";
+import * as VCARD from "./VCARD";
+import * as XSD from "./XSD";
+
+export * from "./C";
+
+export {
+	CS,
+	LDP,
+	RDF,
+	SHACL,
+	VCARD,
+	XSD,
+};
