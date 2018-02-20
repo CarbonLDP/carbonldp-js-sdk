c323f7cab28469a1cfa5d9ee31b34754152e515c
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-02-20 12:32:26 -0600

    Add base interface for model decorators `ModelDecorator`

---
+++ src/ModelDecorator.spec.ts
@@ -0,0 +1,33 @@
+import {
+	hasMethod,
+	hasProperty,
+	interfaze,
+	module,
+	OBLIGATORY,
+	OPTIONAL
+} from "./test/JasmineExtender";
+
+import { ModelDecorator } from "./ModelDecorator";
+
+describe( module( "Carbon/ModelDecorator" ), ():void => {
+
+	describe( interfaze( "Carbon.ModelDecorator.ModelDecorator", [ "T extends object" ], "Interface with the standard methods of a model decorator" ), ():void => {
+
+		it( hasProperty( OBLIGATORY, "TYPE", "string", "The type the decorator interface is for." ), ():void => {
+			const target:ModelDecorator<any>[ "TYPE" ] = "" as string;
+			expect( target ).toBeDefined();
+		} );
+
+		it( hasMethod( OPTIONAL, "isDecorated", [ { name: "object", type: "object" } ], { type: "object is T" } ), ():void => {
+			const target:ModelDecorator<any>[ "isDecorated" ] = ( object ):object is any => false;
+			expect( target ).toBeDefined();
+		} );
+
+		it( hasMethod( OBLIGATORY, "decorate", [ "W extends object" ], [ { name: "object", type: "W" }, { name: "context", type: "Carbon.Context.Context" } ], { type: "W & T" } ), ():void => {
+			const target:ModelDecorator<any>[ "decorate" ] = ( object, context ) => object;
+			expect( target ).toBeDefined();
+		} );
+
+	} );
+
+} );
---
+++ src/ModelDecorator.ts
@@ -0,0 +1,9 @@
+import { Context } from "./Context";
+
+export interface ModelDecorator<T extends object> {
+	TYPE:string;
+
+	isDecorated?( object:object ):object is T;
+
+	decorate<W extends object>( object:W, carbon:Context ):W & T;
+}
