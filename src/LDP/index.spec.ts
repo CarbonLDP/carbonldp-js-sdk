6e4775fddf7b68a8c1b1b6b20aa1d3769d12cf49
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-03-08 11:13:07 -0600

    Add missing docs of reexports in LDP/

--- src/LDP/index.spec.ts
+++ src/LDP/index.spec.ts
@@ -8,17 +8,50 @@ import * as Utils from "../Utils";
 
 import * as LDP from "./";
 
-import { AddMemberAction } from "./AddMemberAction";
-import { CarbonError } from "./CarbonError";
-import { CarbonMap } from "./CarbonMap";
-import { CarbonMapEntry } from "./CarbonMapEntry";
-import { DirectContainer } from "./DirectContainer";
-import { DocumentMetadata } from "./DocumentMetadata";
-import { ErrorResponse } from "./ErrorResponse";
-import { RemoveMemberAction } from "./RemoveMemberAction";
-import { ResponseMetadata } from "./ResponseMetadata";
-import { ValidationError } from "./ValidationError";
-import { VolatileResource } from "./VolatileResource";
+import {
+	AddMemberAction,
+	AddMemberActionFactory,
+} from "./AddMemberAction";
+import {
+	CarbonError,
+	CarbonErrorFactory,
+} from "./CarbonError";
+import {
+	CarbonMap,
+	CarbonMapFactory,
+} from "./CarbonMap";
+import {
+	CarbonMapEntry,
+	CarbonMapEntryFactory,
+} from "./CarbonMapEntry";
+import {
+	DirectContainer,
+	DirectContainerFactory,
+} from "./DirectContainer";
+import {
+	DocumentMetadata,
+	DocumentMetadataFactory,
+} from "./DocumentMetadata";
+import {
+	ErrorResponse,
+	ErrorResponseFactory,
+} from "./ErrorResponse";
+import {
+	RemoveMemberAction,
+	RemoveMemberActionFactory,
+} from "./RemoveMemberAction";
+import {
+	ResponseMetadata,
+	ResponseMetadataFactory,
+} from "./ResponseMetadata";
+import {
+	ValidationError,
+	ValidationErrorFactory,
+} from "./ValidationError";
+import {
+	VolatileResource,
+	VolatileResourceFactory,
+} from "./VolatileResource";
 
 describe( module( "carbonldp/LDP" ), ():void => {
 
@@ -38,11 +71,29 @@ describe( module( "carbonldp/LDP" ), ():void => {
 
 	it( reexports(
 		STATIC,
+		"AddMemberActionFactory",
+		"CarbonLDP.LDP.AddMemberActionFactory"
+	), ():void => {
+		const target:LDP.AddMemberActionFactory = {} as AddMemberActionFactory;
+		expect( target ).toBeDefined();
+	} );
+
+	it( reexports(
+		STATIC,
 		"CarbonMap",
 		"CarbonLDP.LDP.CarbonMap"
 	), ():void => {
 		expect( LDP.CarbonMap ).toBeDefined();
 		expect( LDP.CarbonMap ).toBe( CarbonMap );
+	} );
+
+	it( reexports(
+		STATIC,
+		"CarbonMapFactory",
+		"CarbonLDP.LDP.CarbonMapFactory"
+	), ():void => {
+		const target:LDP.CarbonMapFactory = {} as CarbonMapFactory;
+		expect( target ).toBeDefined();
 	} );
 
 	it( reexports(
@@ -56,11 +107,29 @@ describe( module( "carbonldp/LDP" ), ():void => {
 
 	it( reexports(
 		STATIC,
+		"DirectContainerFactory",
+		"CarbonLDP.LDP.DirectContainerFactory"
+	), ():void => {
+		const target:LDP.DirectContainerFactory = {} as DirectContainerFactory;
+		expect( target ).toBeDefined();
+	} );
+
+	it( reexports(
+		STATIC,
 		"CarbonMapEntry",
 		"CarbonLDP.LDP.CarbonMapEntry"
 	), ():void => {
 		expect( LDP.CarbonMapEntry ).toBeDefined();
 		expect( LDP.CarbonMapEntry ).toBe( CarbonMapEntry );
+	} );
+
+	it( reexports(
+		STATIC,
+		"CarbonMapEntryFactory",
+		"CarbonLDP.LDP.CarbonMapEntryFactory"
+	), ():void => {
+		const target:LDP.CarbonMapEntryFactory = {} as CarbonMapEntryFactory;
+		expect( target ).toBeDefined();
 	} );
 
 	it( reexports(
@@ -74,11 +143,29 @@ describe( module( "carbonldp/LDP" ), ():void => {
 
 	it( reexports(
 		STATIC,
+		"CarbonErrorFactory",
+		"CarbonLDP.LDP.CarbonErrorFactory"
+	), ():void => {
+		const target:LDP.CarbonErrorFactory = {} as CarbonErrorFactory;
+		expect( target ).toBeDefined();
+	} );
+
+	it( reexports(
+		STATIC,
 		"RemoveMemberAction",
 		"CarbonLDP.LDP.RemoveMemberAction"
 	), ():void => {
 		expect( LDP.RemoveMemberAction ).toBeDefined();
 		expect( LDP.RemoveMemberAction ).toBe( RemoveMemberAction );
+	} );
+
+	it( reexports(
+		STATIC,
+		"RemoveMemberActionFactory",
+		"CarbonLDP.LDP.RemoveMemberActionFactory"
+	), ():void => {
+		const target:LDP.RemoveMemberActionFactory = {} as RemoveMemberActionFactory;
+		expect( target ).toBeDefined();
 	} );
 
 	it( reexports(
@@ -92,11 +179,29 @@ describe( module( "carbonldp/LDP" ), ():void => {
 
 	it( reexports(
 		STATIC,
+		"ErrorResponseFactory",
+		"CarbonLDP.LDP.ErrorResponseFactory"
+	), ():void => {
+		const target:LDP.ErrorResponseFactory = {} as ErrorResponseFactory;
+		expect( target ).toBeDefined();
+	} );
+
+	it( reexports(
+		STATIC,
 		"ResponseMetadata",
 		"CarbonLDP.LDP.ResponseMetadata"
 	), ():void => {
 		expect( LDP.ResponseMetadata ).toBeDefined();
 		expect( LDP.ResponseMetadata ).toBe( ResponseMetadata );
+	} );
+
+	it( reexports(
+		STATIC,
+		"ResponseMetadataFactory",
+		"CarbonLDP.LDP.ResponseMetadataFactory"
+	), ():void => {
+		const target:LDP.ResponseMetadataFactory = {} as ResponseMetadataFactory;
+		expect( target ).toBeDefined();
 	} );
 
 	it( reexports(
@@ -110,6 +215,15 @@ describe( module( "carbonldp/LDP" ), ():void => {
 
 	it( reexports(
 		STATIC,
+		"DocumentMetadataFactory",
+		"CarbonLDP.LDP.DocumentMetadataFactory"
+	), ():void => {
+		const target:LDP.DocumentMetadataFactory = {} as DocumentMetadataFactory;
+		expect( target ).toBeDefined();
+	} );
+
+	it( reexports(
+		STATIC,
 		"ValidationError",
 		"CarbonLDP.LDP.ValidationError"
 	), ():void => {
@@ -119,11 +233,29 @@ describe( module( "carbonldp/LDP" ), ():void => {
 
 	it( reexports(
 		STATIC,
+		"ValidationErrorFactory",
+		"CarbonLDP.LDP.ValidationErrorFactory"
+	), ():void => {
+		const target:LDP.ValidationErrorFactory = {} as ValidationErrorFactory;
+		expect( target ).toBeDefined();
+	} );
+
+	it( reexports(
+		STATIC,
 		"VolatileResource",
 		"CarbonLDP.LDP.VolatileResource"
 	), ():void => {
 		expect( LDP.VolatileResource ).toBeDefined();
 		expect( LDP.VolatileResource ).toBe( VolatileResource );
+	} );
+
+	it( reexports(
+		STATIC,
+		"VolatileResourceFactory",
+		"CarbonLDP.LDP.VolatileResourceFactory"
+	), ():void => {
+		const target:LDP.VolatileResourceFactory = {} as VolatileResourceFactory;
+		expect( target ).toBeDefined();
 	} );
 
 } );
