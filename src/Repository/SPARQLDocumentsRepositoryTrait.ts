734b32b7aecfd143bc92e8b64e682673a86154ad
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-07-05 13:37:01 -0500

    Add SPARQLDocumentsRepositoryTrait

---
+++ src/Repository/SPARQLDocumentsRepositoryTrait.ts
@@ -0,0 +1,103 @@
+import { QueryClause } from "sparqler/clauses";
+import {
+	ModelDecorator,
+	ModelPrototype
+} from "../core";
+import { IllegalArgumentError } from "../Errors";
+import { RequestOptions } from "../HTTP";
+import { DigestedObjectSchema } from "../ObjectSchema";
+import {
+	FinishSPARQLSelect,
+	SPARQLBuilder,
+	SPARQLSelectResults,
+	SPARQLService
+} from "../SPARQL";
+import { BaseDocumentsRepository } from "./BaseDocumentsRepository";
+import {
+	_getNotInContextMessage,
+	HTTPRepositoryTrait
+} from "./HTTPRepositoryTrait";
+
+export interface SPARQLDocumentsRepositoryTrait extends HTTPRepositoryTrait {
+	executeASKQuery( uri:string, askQuery:string, requestOptions?:RequestOptions ):Promise<boolean>;
+
+	executeSELECTQuery<T extends object>( uri:string, selectQuery:string, requestOptions?:RequestOptions ):Promise<SPARQLSelectResults<T>>;
+
+	executeUPDATE( uri:string, update:string, requestOptions?:RequestOptions ):Promise<void>;
+
+
+	sparql( uri:string ):QueryClause<FinishSPARQLSelect>;
+}
+
+
+export type SPARQLDocumentsRepositoryTraitFactory =
+	& ModelPrototype<SPARQLDocumentsRepositoryTrait, HTTPRepositoryTrait>
+	& ModelDecorator<SPARQLDocumentsRepositoryTrait, BaseDocumentsRepository>
+	;
+
+export const SPARQLDocumentsRepositoryTrait:SPARQLDocumentsRepositoryTraitFactory = {
+	PROTOTYPE: {
+		executeASKQuery( this:SPARQLDocumentsRepositoryTrait, uri:string, askQuery:string, requestOptions?:RequestOptions ):Promise<boolean> {
+			if( ! this.$context.registry.inScope( uri ) ) return Promise.reject( new IllegalArgumentError( _getNotInContextMessage( uri ) ) );
+			const url:string = this.$context.resolve( uri );
+
+			return SPARQLService
+				.executeASKQuery( url, askQuery, requestOptions )
+				.then( ( [ rawResults ] ) => rawResults )
+				.catch( this._parseFailedResponse.bind( this ) );
+		},
+
+		executeSELECTQuery<T extends object>( this:SPARQLDocumentsRepositoryTrait, uri:string, selectQuery:string, requestOptions?:RequestOptions ):Promise<SPARQLSelectResults<T>> {
+			if( ! this.$context.registry.inScope( uri ) ) return Promise.reject( new IllegalArgumentError( _getNotInContextMessage( uri ) ) );
+			const url:string = this.$context.resolve( uri );
+
+			return SPARQLService
+				.executeSELECTQuery<T>( url, selectQuery, this.$context.registry, requestOptions )
+				.then( ( [ selectResults ] ) => selectResults )
+				.catch( this._parseFailedResponse.bind( this ) );
+		},
+
+		executeUPDATE( this:SPARQLDocumentsRepositoryTrait, uri:string, update:string, requestOptions?:RequestOptions ):Promise<void> {
+			if( ! this.$context.registry.inScope( uri ) ) return Promise.reject( new IllegalArgumentError( _getNotInContextMessage( uri ) ) );
+			const url:string = this.$context.resolve( uri );
+
+			return SPARQLService
+				.executeUPDATE( url, update, requestOptions )
+				.then( () => {} )
+				.catch( this._parseFailedResponse.bind( this ) );
+		},
+
+
+		sparql( this:SPARQLDocumentsRepositoryTrait, uri:string ):QueryClause<FinishSPARQLSelect> {
+			if( ! this.$context.registry.inScope( uri ) ) throw new IllegalArgumentError( _getNotInContextMessage( uri ) );
+			const url:string = this.$context.resolve( uri );
+
+			const schema:DigestedObjectSchema = this.$context.registry.getGeneralSchema();
+			let builder:QueryClause<FinishSPARQLSelect> = new SPARQLBuilder( this, url )
+				.base( schema.base )
+				.vocab( schema.vocab );
+
+			schema.prefixes.forEach( ( name:string, prefix:string ) => {
+				builder = builder.prefix( prefix, name );
+			} );
+
+			return builder;
+		},
+	},
+
+
+	isDecorated( object:object ):object is SPARQLDocumentsRepositoryTrait {
+		return ModelDecorator
+			.hasPropertiesFrom( SPARQLDocumentsRepositoryTrait.PROTOTYPE, object );
+	},
+
+	decorate<T extends object>( object:T ):T & SPARQLDocumentsRepositoryTrait {
+		if( SPARQLDocumentsRepositoryTrait.isDecorated( object ) ) return object;
+
+		const target:T & HTTPRepositoryTrait = ModelDecorator
+			.decorateMultiple( object, HTTPRepositoryTrait );
+
+		return ModelDecorator
+			.definePropertiesFrom( SPARQLDocumentsRepositoryTrait.PROTOTYPE, target );
+	},
+};
--- src/SPARQL/Builder.ts
+++ src/SPARQL/Builder.ts
@@ -4,26 +4,20 @@ import {
 	FinishClause
 } from "sparqler/clauses";
 import { finishDecorator } from "sparqler/clauses/decorators";
-
-import { SPARQLDocument } from "./SPARQLDocument";
-import { SPARQLRawResults } from "./RawResults";
+import { SPARQLDocumentsRepositoryTrait } from "../Repository/SPARQLDocumentsRepositoryTrait";
 import { SPARQLSelectResults } from "./SelectResults";
 
 export interface FinishSPARQLSelect extends FinishClause {
 	execute<T extends object>():Promise<SPARQLSelectResults<T>>;
-
-	executeRaw():Promise<SPARQLRawResults>;
 }
 
 export class SPARQLBuilder extends SPARQLER<FinishSPARQLSelect> {
-	constructor( resource:SPARQLDocument, entryPoint:string ) {
+	constructor( resource:SPARQLDocumentsRepositoryTrait, entryPoint:string ) {
 		super( <W extends object>( container:Container<FinishSPARQLSelect>, object:W ):W & FinishSPARQLSelect => {
 			const finishObject:FinishClause & W = finishDecorator( container, object );
 			return Object.assign( finishObject, {
 				execute: <T extends object>():Promise<SPARQLSelectResults<T>> =>
 					resource.executeSELECTQuery<T>( entryPoint, finishObject.toCompactString() ),
-				executeRaw: ():Promise<SPARQLRawResults> =>
-					resource.executeRawSELECTQuery( entryPoint, finishObject.toCompactString() ),
 			} );
 		} );
 	}
