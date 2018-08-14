97ad7b57a13cdcc076ec90c8cc6dff7e87e96d45
Miguel Aragón <miguel.aragon@base22.com>
2018-08-14 11:04:23 +0900

    v5.0.0-alpha.2

--- src/CarbonLDP.ts
+++ src/CarbonLDP.ts
@@ -77,7 +77,7 @@ export class CarbonLDP extends DocumentsContext {
 	static Utils:typeof Utils = Utils;
 
 
-	static get version():string { return "5.0.0-alpha.1"; }
+	static get version():string { return "5.0.0-alpha.2"; }
 
 	// noinspection JSMethodCanBeStatic
 	get version():string { return CarbonLDP.version; }
