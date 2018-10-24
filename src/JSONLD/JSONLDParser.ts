be5448fb4a57ab07a5f2b173bfd10701ba80185d
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-10-24 18:06:28 -0500

    Add docs comments to JSONLD/*

--- src/JSONLD/JSONLDParser.ts
+++ src/JSONLD/JSONLDParser.ts
@@ -2,7 +2,14 @@ import { JSONParser } from "../HTTP/JSONParser";
 import { Parser } from "../HTTP/Parser";
 import { JSONLDProcessor } from "./JSONLDProcessor";
 
+/**
+ * Parser that normalises JSON-LD objects into a complete expanded form.
+ */
 export class JSONLDParser extends JSONParser implements Parser<object[]> {
+	/**
+	 * Parse the string provided using the {@link JSONLDProcessor.expand}` method.
+	 * @param input The JSON-LD string to parse.
+	 */
 	parse( input:string ):Promise<object[]> {
 		return super.parse( input ).then( JSONLDProcessor.expand );
 	}
