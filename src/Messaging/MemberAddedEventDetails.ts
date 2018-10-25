f58a5dbe6f5d05cc73873c6379d2cbc4983bb2a0
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-10-25 15:05:36 -0500

    Add docs comments to Messaging/*

--- src/Messaging/MemberAddedEventDetails.ts
+++ src/Messaging/MemberAddedEventDetails.ts
@@ -5,10 +5,17 @@ import { C } from "../Vocabularies/C";
 import { MemberEventDetails } from "./MemberEventDetails";
 
 
+/**
+ * Model that represents a `c:MemberAddedEventDetails`.
+ */
 export interface MemberAddedEventDetails extends MemberEventDetails {
 }
 
 
+// TODO: Change to type-alias
+/**
+ * Factory, decorator and utils for {@link MemberAddedEventDetails}.
+ */
 export interface MemberAddedEventDetailsFactory {
 	TYPE:string;
 	SCHEMA:ObjectSchema;
@@ -17,8 +24,10 @@ export interface MemberAddedEventDetailsFactory {
 const TYPE:string = C.MemberAddedEventDetails;
 const SCHEMA:ObjectSchema = MemberEventDetails.SCHEMA;
 
+/**
+ * Constant that implements {@link MemberAddedEventDetailsFactory}.
+ */
 export const MemberAddedEventDetails:MemberAddedEventDetailsFactory = {
 	TYPE,
 	SCHEMA,
 };
-
