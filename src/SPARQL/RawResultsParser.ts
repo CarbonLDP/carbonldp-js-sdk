9f1bb9871dcaac1c443ef5c9161d3e10afde887a
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-10-29 13:29:35 -0600

    Add docs comments to SPARQL/*

--- src/SPARQL/RawResultsParser.ts
+++ src/SPARQL/RawResultsParser.ts
@@ -4,7 +4,14 @@ import { Parser } from "../HTTP/Parser";
 import { SPARQLRawResults } from "./RawResults";
 
 
+/**
+ * Class to parse SPARQL Query result to a {@link SPARQLRawResults} object.
+ */
 export class SPARQLRawResultsParser extends JSONParser implements Parser<SPARQLRawResults> {
+	/**
+	 * Parse the SPARQL Query string result to a {@link SPARQLRawResults} object.
+	 * @param input The string to parse.
+	 */
 	parse( input:string ):Promise<SPARQLRawResults> {
 		return super.parse( input ).then( object => object as SPARQLRawResults );
 	}
