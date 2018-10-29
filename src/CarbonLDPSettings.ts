fef72b575735d2f7bed2fdd4a59663754af7d5ac
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-10-29 14:01:41 -0600

    Add docs comments to CarbonLDPSettings interface

--- src/CarbonLDPSettings.ts
+++ src/CarbonLDPSettings.ts
@@ -1,8 +1,21 @@
 import { DocumentsContextSettings } from "./Context/DocumentsContextSettings";
 
 
+/**
+ * Interface of the possible settings used by the Carbon class.
+ */
 export interface CarbonLDPSettings extends DocumentsContextSettings {
+	/**
+	 * The host of the platform to connect to.
+	 */
 	host:string;
+	/**
+	 * The optional port of the host to connect to.
+	 */
 	port?:number;
+	/**
+	 * Flag that indicates is the server is under a secure connection or not.
+	 * By default it will be set to true, making the host to be resolved as `https://`
+	 */
 	ssl?:boolean;
 }
