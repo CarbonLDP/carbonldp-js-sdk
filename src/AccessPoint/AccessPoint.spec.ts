42e1e34522d66c206d108666759200ac7239b40a
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-11-14 12:11:47 -0600

    Clean AccessPoint/* docs tests

--- src/AccessPoint/AccessPoint.spec.ts
+++ src/AccessPoint/AccessPoint.spec.ts
@@ -1,85 +1,30 @@
 import { Document } from "../Document/Document";
 
-import {
-	extendsClass,
-	hasProperty,
-	hasSignature,
-	interfaze,
-	method,
-	module,
-	OBLIGATORY,
-	property,
-	STATIC,
-} from "../test/JasmineExtender";
-
 import { AccessPoint, AccessPointFactory } from "./AccessPoint";
-import { TransientAccessPoint, TransientAccessPointFactory } from "./TransientAccessPoint";
+import { TransientAccessPoint } from "./TransientAccessPoint";
 
 
-describe( module( "carbonldp/AccessPoint" ), ():void => {
+describe( "AccessPoint", () => {
 
-	describe( interfaze(
-		"CarbonLDP.AccessPoint",
-		"Interface that represents a persisted Carbon LDP AccessPoint."
-	), ():void => {
-
-		it( extendsClass( "CarbonLDP.TransientAccessPoint" ), ():void => {} );
-		it( extendsClass( "CarbonLDP.Document" ), ():void => {} );
-
-		it( hasProperty(
-			OBLIGATORY,
-			"membershipResource",
-			"CarbonLDP.Document",
-			"The membership resource the access point belongs to."
-		), ():void => {} );
-
-		it( hasProperty(
-			OBLIGATORY,
-			"hasMemberRelation",
-			"CarbonLDP.Pointer",
-			"The member relation of the access point manages."
-		), ():void => {} );
-
-		it( hasProperty(
-			OBLIGATORY,
-			"isMemberOfRelation",
-			"CarbonLDP.Pointer",
-			"The inverted relation of the access point."
-		), ():void => {} );
-
-		it( hasProperty(
-			OBLIGATORY,
-			"insertedContentRelation",
-			"CarbonLDP.Pointer",
-			"The inserted content relation of the access point."
-		), ():void => {} );
-
+	it( "should exists", () => {
+		expect( AccessPoint ).toBeDefined();
+		expect( AccessPoint ).toEqual( jasmine.any( Object ) );
 	} );
 
-	describe( interfaze(
-		"CarbonLDP.AccessPointFactory",
-		"Interface with the factory, decorate and utils methods of a `CarbonLDP.AccessPointFactory` object."
-	), ():void => {
 
-		it( extendsClass( "CarbonLDP.TransientAccessPointFactory" ), () => {
-			const target:TransientAccessPointFactory = AccessPoint as AccessPointFactory;
-			expect( target ).toEqual( jasmine.objectContaining( {
+	describe( "AccessPointFactory", () => {
+
+		it( "should inherit TransientAccessPointFactory", () => {
+			expect( AccessPoint ).toEqual( jasmine.objectContaining( {
 				TYPE: TransientAccessPoint.TYPE,
 				create: TransientAccessPoint.create,
 				createFrom: TransientAccessPoint.createFrom,
 			} ) );
 		} );
 
-		describe( method( OBLIGATORY, "is" ), ():void => {
+		describe( "AccessPoint.is", () => {
 
-			it( hasSignature(
-				"Returns true if the object provided is considered a `CarbonLDP.AccessPoint` object", [
-					{ name: "value", type: "any" },
-				],
-				{ type: "value is CarbonLDP.AccessPoint" }
-			), ():void => {} );
-
-			it( "should exists", ():void => {
+			it( "should exists", () => {
 				expect( AccessPoint.is ).toBeDefined();
 				expect( AccessPoint.is ).toEqual( jasmine.any( Function ) );
 			} );
@@ -87,7 +32,7 @@ describe( module( "carbonldp/AccessPoint" ), ():void => {
 
 			let isTransientAccessPoint:jasmine.Spy;
 			let isDocument:jasmine.Spy;
-			beforeEach( ():void => {
+			beforeEach( () => {
 				isTransientAccessPoint = spyOn( TransientAccessPoint, "is" )
 					.and.returnValue( true );
 				isDocument = spyOn( Document, "is" )
@@ -113,19 +58,4 @@ describe( module( "carbonldp/AccessPoint" ), ():void => {
 
 	} );
 
-	describe( property(
-		STATIC,
-		"AccessPoint",
-		"CarbonLDP.AccessPointFactory",
-		"Constant that implements the `CarbonLDP.AccessPointFactory` interface."
-	), ():void => {
-
-		it( "should exists", ():void => {
-			expect( AccessPoint ).toBeDefined();
-			expect( AccessPoint ).toEqual( jasmine.any( Object ) );
-		} );
-
-	} );
-
 } );
-
--- src/AccessPoint/TransientAccessPoint.spec.ts
+++ src/AccessPoint/TransientAccessPoint.spec.ts
@@ -2,87 +2,32 @@ import { TransientDirectContainer } from "../LDP/DirectContainer/TransientDirect
 
 import { Pointer } from "../Pointer/Pointer";
 
-import {
-	extendsClass,
-	hasMethod,
-	hasProperty,
-	hasSignature,
-	interfaze,
-	isDefined,
-	method,
-	module,
-	OBLIGATORY,
-	OPTIONAL,
-	property,
-	STATIC,
-} from "../test/JasmineExtender";
-
 import { C } from "../Vocabularies/C";
 
 import { BaseAccessPoint } from "./BaseAccessPoint";
 import { TransientAccessPoint } from "./TransientAccessPoint";
 
 
-describe( module( "carbonldp/AccessPoint" ), ():void => {
+describe( "TransientAccessPoint", () => {
 
-	describe( interfaze(
-		"CarbonLDP.TransientAccessPoint",
-		"Interface that represents the document of an in-memory access point."
-	), ():void => {
-
-		it( extendsClass( "CarbonLDP.LDP.TransientDirectContainer" ), ():void => {} );
-
-		it( hasProperty(
-			OBLIGATORY,
-			"hasMemberRelation",
-			"CarbonLDP.Pointer",
-			"Pointer that represents the member relation that the access point will manage."
-		), ():void => {} );
-
-		it( hasProperty(
-			OPTIONAL,
-			"isMemberOfRelation",
-			"CarbonLDP.Pointer",
-			"Pointer that represents the inverted relation that the access point will create."
-		), ():void => {} );
-
-		it( hasProperty(
-			OPTIONAL,
-			"insertedContentRelation",
-			"CarbonLDP.Pointer",
-			"Pointer that represents the inserted content relation of the access point."
-		), ():void => {} );
-
+	it( "should exists", () => {
+		expect( TransientAccessPoint ).toBeDefined();
+		expect( TransientAccessPoint ).toEqual( jasmine.any( Object ) );
 	} );
 
-	describe( interfaze(
-		"CarbonLDP.TransientAccessPointFactory",
-		"Interface with the factory, decorate and utils methods of a `CarbonLDP.TransientAccessPointFactory` object."
-	), ():void => {
 
-		it( hasProperty(
-			OBLIGATORY,
-			"TYPE",
-			"CarbonLDP.Vocabularies.C.AccessPoint"
-		), ():void => {} );
+	describe( "TransientAccessPointFactory", () => {
 
-		describe( method( OBLIGATORY, "is" ), ():void => {
+		describe( "TransientAccessPoint.is", () => {
 
-			it( hasSignature(
-				"Returns true if the object provided is considered a `CarbonLDP.TransientAccessPoint` object", [
-					{ name: "value", type: "any" },
-				],
-				{ type: "value is CarbonLDP.TransientAccessPoint" }
-			), ():void => {} );
-
-			it( "should exists", ():void => {
+			it( "should exists", () => {
 				expect( TransientAccessPoint.is ).toBeDefined();
 				expect( TransientAccessPoint.is ).toEqual( jasmine.any( Function ) );
 			} );
 
 
 			let isTransientDirectContainer:jasmine.Spy;
-			beforeEach( ():void => {
+			beforeEach( () => {
 				isTransientDirectContainer = spyOn( TransientDirectContainer, "is" )
 					.and.returnValue( true );
 			} );
@@ -99,61 +44,27 @@ describe( module( "carbonldp/AccessPoint" ), ():void => {
 
 		} );
 
-		it( hasMethod(
-			OBLIGATORY,
-			"create",
-			[ "T extends object" ],
-			"Creates a `CarbonLDP.TransientAccessPoint` object with the parameters specified.", [
-				{ name: "data", type: "T & CarbonLDP.BaseAccessPoint", description: "Data necessary to create an access point." },
-			],
-			{ type: "CarbonLDP.TransientAccessPoint" }
-		), ():void => {} );
+		describe( "TransientAccessPoint.TYPE", () => {
 
-		it( hasMethod(
-			OBLIGATORY,
-			"createFrom",
-			[ "T extends object" ],
-			"Creates a `CarbonLDP.TransientAccessPoint` object from the object and parameters specified.", [
-				{ name: "object", type: "T & CarbonLDP.BaseAccessPoint", description: "Object that will be converted into an AccessPoint." },
-			],
-			{ type: "T & CarbonLDP.TransientAccessPoint" }
-		), ():void => {} );
-
-	} );
-
-	describe( property(
-		STATIC,
-		"TransientAccessPoint",
-		"CarbonLDP.TransientAccessPointFactory",
-		"Constant that implements the `CarbonLDP.TransientAccessPointFactory` interface."
-	), ():void => {
-
-		it( isDefined(), ():void => {
-			expect( TransientAccessPoint ).toBeDefined();
-			expect( TransientAccessPoint ).toEqual( jasmine.any( Object ) );
-		} );
-
-		describe( "TransientAccessPoint.TYPE", ():void => {
-
-			it( "should exists", ():void => {
+			it( "should exists", () => {
 				expect( TransientAccessPoint.TYPE ).toBeDefined();
 				expect( TransientAccessPoint.TYPE ).toEqual( jasmine.any( String ) );
 			} );
 
-			it( "should be c:AccessPoint", ():void => {
+			it( "should be c:AccessPoint", () => {
 				expect( TransientAccessPoint.TYPE ).toBe( C.AccessPoint );
 			} );
 
 		} );
 
-		describe( "TransientAccessPoint.create", ():void => {
+		describe( "TransientAccessPoint.create", () => {
 
-			it( "should exists", ():void => {
+			it( "should exists", () => {
 				expect( TransientAccessPoint.create ).toBeDefined();
 				expect( TransientAccessPoint.create ).toEqual( jasmine.any( Function ) );
 			} );
 
-			it( "should call TransientAccessPoint.createFrom", ():void => {
+			it( "should call TransientAccessPoint.createFrom", () => {
 				const spy:jasmine.Spy = spyOn( TransientAccessPoint, "createFrom" );
 
 				const base:BaseAccessPoint = {
@@ -165,7 +76,7 @@ describe( module( "carbonldp/AccessPoint" ), ():void => {
 				expect( spy ).toHaveBeenCalledWith( base );
 			} );
 
-			it( "should return different reference", ():void => {
+			it( "should return different reference", () => {
 				const base:BaseAccessPoint = {
 					membershipResource: Pointer.create(),
 					hasMemberRelation: "http://example.com/myNamespace#some-relation",
@@ -177,14 +88,14 @@ describe( module( "carbonldp/AccessPoint" ), ():void => {
 
 		} );
 
-		describe( "TransientAccessPoint.createFrom", ():void => {
+		describe( "TransientAccessPoint.createFrom", () => {
 
-			it( "should exists", ():void => {
+			it( "should exists", () => {
 				expect( TransientAccessPoint.createFrom ).toBeDefined();
 				expect( TransientAccessPoint.createFrom ).toEqual( jasmine.any( Function ) );
 			} );
 
-			it( "should call TransientDirectContainer.createFrom", ():void => {
+			it( "should call TransientDirectContainer.createFrom", () => {
 				const spy:jasmine.Spy = spyOn( TransientDirectContainer, "createFrom" )
 					.and.callThrough();
 
@@ -197,7 +108,7 @@ describe( module( "carbonldp/AccessPoint" ), ():void => {
 				expect( spy ).toHaveBeenCalledWith( base );
 			} );
 
-			it( "should return the same reference", ():void => {
+			it( "should return the same reference", () => {
 				const base:BaseAccessPoint = {
 					membershipResource: Pointer.create(),
 					hasMemberRelation: "http://example.com/myNamespace#some-relation",
