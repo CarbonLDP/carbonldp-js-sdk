d57392ba2d911b941aa1ec0f4a094367361f5149
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-11-21 16:26:39 -0600

    Clean and refactor Pointer/* docs tests

--- src/Pointer/Pointer.spec.ts
+++ src/Pointer/Pointer.spec.ts
@@ -1,185 +1,104 @@
 import { createNonEnumerable } from "../../test/helpers/miscellaneous";
 
-import { ModelDecorator } from "../Model/ModelDecorator";
-import { ModelFactory } from "../Model/ModelFactory";
-import { ModelPrototype } from "../Model/ModelPrototype";
-import { ModelTypeGuard } from "../Model/ModelTypeGuard";
-
-import {
-	extendsClass,
-	hasMethod,
-	hasProperty,
-	interfaze,
-	module,
-	OBLIGATORY,
-	property,
-	STATIC,
-} from "../test/JasmineExtender";
-
-import { BasePointer } from "./BasePointer";
 import { Pointer, PointerFactory } from "./Pointer";
 
 
-describe( module( "carbonldp/Pointer" ), ():void => {
+describe( "Pointer", () => {
 
-	describe( interfaze(
-		"CarbonLDP.Pointer",
-		"Interface that represents any element that can be referenced by an URI."
-	), ():void => {
-
-		it( hasProperty(
-			OBLIGATORY,
-			"$id",
-			"string",
-			"The URI that identifies the pointer."
-		), ():void => {} );
-
+	it( "should exist", () => {
+		expect( Pointer ).toBeDefined();
+		expect( Pointer ).toEqual( jasmine.any( Object ) );
 	} );
 
-	describe( interfaze(
-		"CarbonLDP.PointerFactory",
-		"Interface with the factory, decorate, and utils methods of a `CarbonLDP.Pointer` object."
-	), ():void => {
 
-		it( extendsClass( "CarbonLDP.Model.ModelPrototype<CarbonLDP.Pointer>" ), () => {
-			const target:ModelPrototype<Pointer> = {} as PointerFactory;
-			expect( target ).toBeDefined();
-		} );
+	describe( "[[factory]]", () => {
 
-		it( extendsClass( "CarbonLDP.Model.ModelDecorator<CarbonLDP.Pointer, CarbonLDP.BasePointer>" ), () => {
-			const target:ModelDecorator<Pointer, BasePointer> = {} as PointerFactory;
-			expect( target ).toBeDefined();
-		} );
+		describe( "Pointer.isDecorated", () => {
 
-		it( extendsClass( "CarbonLDP.Model.ModelTypeGuard<CarbonLDP.Pointer>" ), () => {
-			const target:ModelTypeGuard<Pointer> = {} as PointerFactory;
-			expect( target ).toBeDefined();
-		} );
-
-		it( extendsClass( "CarbonLDP.Model.ModelFactory<CarbonLDP.Pointer, CarbonLDP.BasePointer>" ), () => {
-			const target:ModelFactory<Pointer, BasePointer> = {} as PointerFactory;
-			expect( target ).toBeDefined();
-		} );
-
-		it( hasMethod(
-			OBLIGATORY,
-			"create",
-			[ "T extends object" ],
-			"Creates a Pointer object with the ID provided.", [
-				{ name: "data", type: "T & CarbonLDP.BasePointer", optional: true },
-			],
-			{ type: "T & CarbonLDP.Pointer" }
-		), ():void => {} );
-
-
-		it( hasMethod(
-			OBLIGATORY,
-			"areEqual",
-			"Check of the objects refer to the same resource by its ID.", [
-				{ name: "pointer1", type: "CarbonLDP.Pointer" },
-				{ name: "pointer2", type: "CarbonLDP.Pointer" },
-			],
-			{ type: "boolean" }
-		), ():void => {} );
-
-		it( hasMethod(
-			OBLIGATORY,
-			"getIDs",
-			"Extracts the IDs of all the pointers provided.", [
-				{ name: "pointers", type: "CarbonLDP.Pointer[]", description: "The array of Pointers to obtain their IDs." },
-			],
-			{ type: "string[]" }
-		), ():void => {} );
-
-	} );
-
-	describe( property(
-		STATIC,
-		"Pointer",
-		"CarbonLDP.PointerFactory",
-		"Constant that implements the `CarbonLDP.PointerFactory` interface."
-	), ():void => {
-
-		it( "should exist", ():void => {
-			expect( Pointer ).toBeDefined();
-			expect( Pointer ).toEqual( jasmine.any( Object ) );
-		} );
-
-
-		describe( "Pointer.isDecorated", ():void => {
-
-			it( "should exist", ():void => {
+			it( "should exist", () => {
 				expect( Pointer.isDecorated ).toBeDefined();
 				expect( Pointer.isDecorated ).toEqual( jasmine.any( Function ) );
 			} );
 
-			// TODO: Separate in different tests
-			it( "should test method", ():void => {
-				const pointer:PointerFactory[ "PROTOTYPE" ] = createNonEnumerable( {
+			let mockObject:PointerFactory[ "PROTOTYPE" ];
+			beforeEach( () => {
+				mockObject = createNonEnumerable( {
 					$id: null,
 				} );
-				expect( Pointer.isDecorated( pointer ) ).toBe( true );
+			} );
 
-				delete pointer.$id;
-				expect( Pointer.isDecorated( pointer ) ).toBe( false );
-				pointer.$id = null;
+
+			it( "should return true when all properties", () => {
+				expect( Pointer.isDecorated( mockObject ) ).toBe( true );
+			} );
+
+			it( "should return false when no `$id`", () => {
+				delete mockObject.$id;
+				expect( Pointer.isDecorated( mockObject ) ).toBe( false );
+				mockObject.$id = null;
 			} );
 
 		} );
 
-		describe( "Pointer.is", ():void => {
+		describe( "Pointer.is", () => {
 
-			it( "should exist", ():void => {
+			it( "should exist", () => {
 				expect( Pointer.is ).toBeDefined();
 				expect( Pointer.is ).toEqual( jasmine.any( Function ) );
 			} );
 
-			// TODO: Separate in different tests
-			it( "should test method", ():void => {
+
+			it( "should return false when undefined", () => {
 				expect( Pointer.is( undefined ) ).toBe( false );
 				expect( Pointer.is( null ) ).toBe( false );
-				expect( Pointer.is( {} ) ).toBe( false );
+			} );
 
+			it( "should return false when empty", () => {
+				expect( Pointer.is( {} ) ).toBe( false );
+			} );
+
+			it( "should return true when non-enumerable properties", () => {
 				const target:Pointer = createNonEnumerable( {
 					$id: null,
 				} );
+
 				expect( Pointer.is( target ) ).toBe( true );
 			} );
 
 		} );
 
-		describe( "Pointer.create", ():void => {
+		describe( "Pointer.create", () => {
 
-			it( "should exist", ():void => {
+			it( "should exist", () => {
 				expect( Pointer.create ).toBeDefined();
 				expect( Pointer.create ).toEqual( jasmine.any( Function ) );
 			} );
 
 
-			it( "should maintain the `$id` in `Pointer.$id`", ():void => {
+			it( "should maintain the `$id` in `Pointer.$id`", () => {
 				const pointer:Pointer = Pointer.create( { $id: "https://example.com/pointer/" } );
 				expect( pointer.$id ).toBe( "https://example.com/pointer/" );
 			} );
 
-			it( "should set empty string in `Pointer.$id` if no `$id` provided", ():void => {
+			it( "should set empty string in `Pointer.$id` if no `$id` provided", () => {
 				const pointer:Pointer = Pointer.create( {} );
 				expect( pointer.$id ).toBe( "" );
 			} );
 
-			it( "should set empty string in `Pointer.$id` none provided", ():void => {
+			it( "should set empty string in `Pointer.$id` none provided", () => {
 				const pointer:Pointer = Pointer.create();
 				expect( pointer.$id ).toBe( "" );
 			} );
 
 
-			it( "should return different reference", ():void => {
+			it( "should return different reference", () => {
 				const object:{} = {};
 				const returned:Pointer = Pointer.create( {} );
 
 				expect( object ).not.toBe( returned );
 			} );
 
-			it( "should call Pointer.createFrom", ():void => {
+			it( "should call Pointer.createFrom", () => {
 				const spy:jasmine.Spy = spyOn( Pointer, "createFrom" );
 
 				Pointer.create( { the: "data", $id: "" } );
@@ -188,33 +107,33 @@ describe( module( "carbonldp/Pointer" ), ():void => {
 
 		} );
 
-		describe( "Pointer.createFrom", ():void => {
+		describe( "Pointer.createFrom", () => {
 
-			it( "should exist", ():void => {
+			it( "should exist", () => {
 				expect( Pointer.createFrom ).toBeDefined();
 				expect( Pointer.createFrom ).toEqual( jasmine.any( Function ) );
 			} );
 
 
-			it( "should assign the `$id` in `Pointer.$id`", ():void => {
+			it( "should assign the `$id` in `Pointer.$id`", () => {
 				const pointer:Pointer = Pointer.createFrom( { $id: "https://example.com/pointer/" } );
 				expect( pointer.$id ).toBe( "https://example.com/pointer/" );
 			} );
 
-			it( "should set empty string in `Pointer.$id` if no `$id` provided", ():void => {
+			it( "should set empty string in `Pointer.$id` if no `$id` provided", () => {
 				const pointer:Pointer = Pointer.createFrom( {} );
 				expect( pointer.$id ).toBe( "" );
 			} );
 
 
-			it( "should return same reference", ():void => {
+			it( "should return same reference", () => {
 				const object:{} = {};
 				const returned:Pointer = Pointer.createFrom( object );
 
 				expect( object ).toBe( returned );
 			} );
 
-			it( "should call Pointer.decorate", ():void => {
+			it( "should call Pointer.decorate", () => {
 				const spy:jasmine.Spy = spyOn( Pointer, "decorate" );
 
 				Pointer.create( { the: "data", $id: "" } );
@@ -223,35 +142,35 @@ describe( module( "carbonldp/Pointer" ), ():void => {
 
 		} );
 
-		describe( "Pointer.decorate", ():void => {
+		describe( "Pointer.decorate", () => {
 
-			it( "should exist", ():void => {
+			it( "should exist", () => {
 				expect( Pointer.decorate ).toBeDefined();
 				expect( Pointer.decorate ).toEqual( jasmine.any( Function ) );
 			} );
 
 
-			it( "should work with Pointer.isDecorated", ():void => {
+			it( "should work with Pointer.isDecorated", () => {
 				const pointer:Pointer = Pointer.decorate( {} );
 				expect( Pointer.isDecorated( pointer ) ).toBe( true );
 			} );
 
 
-			it( "should assign empty string to `Pointer.$id` when not exists", ():void => {
+			it( "should assign empty string to `Pointer.$id` when not exists", () => {
 				const pointer:Pointer = Pointer.decorate( {} );
 				expect( pointer.$id ).toBe( "" );
 			} );
 
-			it( "should keep ID in `Pointer.$id` when `$id` already exists", ():void => {
+			it( "should keep ID in `Pointer.$id` when `$id` already exists", () => {
 				const pointer:Pointer = Pointer.decorate( { $id: "https://example.com/pointer/" } );
 				expect( pointer.$id ).toBe( "https://example.com/pointer/" );
 			} );
 
 		} );
 
-		describe( "Pointer.areEqual", ():void => {
+		describe( "Pointer.areEqual", () => {
 
-			it( "should exist", ():void => {
+			it( "should exist", () => {
 				expect( Pointer.areEqual ).toBeDefined();
 				expect( Pointer.areEqual ).toEqual( jasmine.any( Function ) );
 			} );
@@ -276,14 +195,14 @@ describe( module( "carbonldp/Pointer" ), ():void => {
 
 		} );
 
-		describe( "Pointer.getIDs", ():void => {
+		describe( "Pointer.getIDs", () => {
 
-			it( "should exist", ():void => {
+			it( "should exist", () => {
 				expect( Pointer.getIDs ).toBeDefined();
 				expect( Pointer.getIDs ).toEqual( jasmine.any( Function ) );
 			} );
 
-			it( "should return the IDs", ():void => {
+			it( "should return the IDs", () => {
 				const pointers:Pointer[] = [
 					Pointer.create( { $id: "http://example.com/resource-1/" } ),
 					Pointer.create( { $id: "http://example.com/resource-2/" } ),
