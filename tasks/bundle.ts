184a1b08d7ce9f30860edb1aaee42776ff1451cf
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2019-02-27 14:30:38 -0600

    Change bundle to use rollup

--- tasks/bundle.ts
+++ tasks/bundle.ts
@@ -1,15 +1,61 @@
 import gulp from "gulp";
 import path from "path";
-import webpack from "webpack";
+import { rollup, Plugin } from "rollup";
+import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import replace from "rollup-plugin-replace";
+import { uglify } from "rollup-plugin-uglify";
+import config from "./config";
 
+const SRC_DIR:string = path.resolve( __dirname, "../build" );
 
-export const bundleSFX:gulp.TaskFunction = ( done ) => {
-	const configPath:string = path.resolve( process.cwd(), "webpack.config.js" );
-	const compiler:webpack.Compiler = webpack( require( configPath ) );
+const DEFAULT_PLUGINS:Plugin[] = [
+	resolve( {
+		main: true,
+		module: true,
+		browser: true,
+	} ),
+	commonjs( {
+		ignoreGlobal: true,
+		sourceMap: false,
+	} ),
+	typescript( {
+		tsconfig: "tsconfig.json",
+		tsconfigOverride: {
+			compilerOptions: {
+				"module": "esnext",
+			},
+		},
+	} ),
+	replace( {
+		"process.env.NODE_ENV": JSON.stringify( "production" ),
+	} ),
+];
 
-	compiler.run( ( error ) => {
-		if( error ) done( error );
-		else done();
-	} );
+export const bundleSFX:gulp.TaskFunction = async () => {
+	// Main bundle
+	await rollup( {
+		input: path.resolve( SRC_DIR, "sfx.ts" ),
+		plugins: DEFAULT_PLUGINS,
+	} ).then( bundle => bundle.write( {
+		file: `${ config.dist.bundle }CarbonLDP.sfx.js`,
+		format: "umd",
+		name: "CarbonLDP",
+	} ) );
+
+	// Minified bundle
+	await rollup( {
+		input: path.resolve( SRC_DIR, "sfx.ts" ),
+		plugins: [
+			...DEFAULT_PLUGINS,
+			uglify(),
+		],
+	} ).then( bundle => bundle.write( {
+		file: `${ config.dist.bundle }CarbonLDP.sfx.min.js`,
+		format: "umd",
+		name: "CarbonLDP",
+		compact: true,
+	} ) );
 };
 bundleSFX.displayName = "bundle:sfx";
