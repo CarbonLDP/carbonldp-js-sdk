58e0563d31ecd832e41328ddffe8b33239752cf8
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2019-03-06 16:37:42 -0600

    Automate API docs deployment

--- tasks/documentation.ts
+++ tasks/documentation.ts
@@ -2,11 +2,17 @@ import { Dgeni } from "dgeni";
 import docsGenerator from "docs-generator";
 import gulp from "gulp";
 import htmlMin from "gulp-htmlmin";
+import path from "path";
 
+
+const docsPath:string = "api-docs/";
 
 export const compileDgeni:gulp.TaskFunction = () => {
 	const dgeni:Dgeni = new Dgeni( [
 		docsGenerator
+			.config( function( writeFilesProcessor:any ):void {
+				writeFilesProcessor.outputFolder = path.resolve( docsPath );
+			} )
 			.config( function( templateFinder:any, templateEngine:any ):void {
 				// Configure pattern for Handlebars templates
 				templateFinder.templateFolders = [ "build/docs/templates/html/" ];
@@ -21,13 +27,13 @@ compileDgeni.displayName = "compile:dgeni";
 
 
 export const documentationMinify:gulp.TaskFunction = () => {
-	return gulp.src( "docs/index.html" )
+	return gulp.src( `${ docsPath }index.html` )
 		.pipe( htmlMin( {
 			collapseWhitespace: true,
 			conservativeCollapse: true,
 			removeComments: true,
 		} ) )
-		.pipe( gulp.dest( "docs" ) )
+		.pipe( gulp.dest( docsPath ) )
 		;
 };
 documentationMinify.displayName = "documentation:minify";
